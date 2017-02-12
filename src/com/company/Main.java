3a09f8b03dc8452cf9d6ce8ae0a974f3b445e1d4
Ian Morrissey <morrisseyi75@gmail.com>
2017-02-11 21:03:55 -0600

    Inital Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int test;
+        System.out.println("Input an integer!");
+        test = keyboard.nextInt();
+
+        if(test == 1){
+            System.out.println("Hello, world!");
+        }
+        else{
+            System.out.println("That's not the right thing!");
+        }
+
+   }
+}
